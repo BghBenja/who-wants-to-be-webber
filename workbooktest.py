168b86983e71566f449c77f7346673af5af28ab9
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2021-11-26 13:50:01 +0100

    questions named for the users

--- workbooktest.py
+++ workbooktest.py
@@ -36,29 +36,44 @@ def random_choice(questions):
     return random.choice(questions)
 
 
-def the_final_countdowning(question):
+def the_final_countdowning(question, player_name):
     counter = 30
     while counter != 0:
         clean()
-        print_question(question)
-        print(f'\033[91mThe answer will be revaild in\033[0m \033[9{counter % 7}m{counter}\033[0m \033[91msecond\033[0m')
+        print_question(question, player_name)
+        print(f'\033[94mThe answer will be revaild in\033[0m \033[9{counter % 7}m{counter}\033[0m \033[94msecond\033[0m')
         counter -= 1
         time.sleep(1)
 
-def print_question(question):
-    print(f'\n\033[94mYour question is:\033[0m\n\n\033[95m{question}\033[0m')
+def print_question(question, player_name):
+    print(f"\n\033[94m{player_name}'s question is:\033[0m\n\n\033[95m{question}\033[0m")
+
+
+def get_players():
+    players = []
+    player_input = 'i'
+    while player_input != '':
+        clean()
+        player_input = input('\033[94mPlayer name:\033[0m\n\n')
+        if player_input != '':
+            players.append(player_input)
+    return players
+
 
 def main():
     question_list = list_maker()
     answer_list = get_answers_list()
+    players_list = get_players()
+    which_player = 0
     try:
         while True:
+            player_name = players_list[which_player % len(players_list)].capitalize()
             question = random_choice(question_list)
-            the_final_countdowning(question)
+            the_final_countdowning(question, player_name)
             clean()
-            print_question(question)
+            print_question(question, player_name)
             print(f'\033[94mYour answer is:\033[0m\n\n\033[95m{answer_list[question_list.index(question)]}\033[0m')
-            input('\033[91mPress enter\033[0m')
+            input('\033[94mPress enter\033[0m')
     except KeyboardInterrupt:
         clean()
         print('\n\033[96mFUCK YOU TONNY!!!!\033[0m\n')
